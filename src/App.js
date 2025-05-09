aa01e65d831b816ec517e5a28a01f63c6babbfe2
Pratham Parmar <prathamparmar.dev@gmail.com>
2025-05-09 21:48:05 +0530

    PROFILE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ConfigProvider } from 'antd';
 import Dashboard from "./components/Dashboard";
 import SignUpSignIn from "./components/Signup";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignUpSignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <ConfigProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignUpSignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </ConfigProvider>
   );
 }
 
